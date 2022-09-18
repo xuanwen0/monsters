3bf27d5bc72f2cab94c58b79c2740eb56e0f1241
10846028 <tony960281@gmail.com>
2022-09-18 14:34:04 +0800

    解答之書更新

--- front-end/monsters_front_end/lib/pages/answerbook.dart
+++ front-end/monsters_front_end/lib/pages/answerbook.dart
@@ -4,6 +4,7 @@ import 'package:adobe_xd/adobe_xd.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:monsters_front_end/model/answerbookModel.dart';
+import 'package:monsters_front_end/pages/style.dart';
 import 'package:monsters_front_end/repository/answerBookRepo.dart';
 
 import 'interaction.dart';
@@ -28,9 +29,22 @@ class _AnswerbookPageState extends State<AnswerbookPage> {
   Widget build(BuildContext context) {
     getAnswer().then((value) => answer = value.content);
     return Scaffold(
-      backgroundColor: const Color(0xfffffed4),
+      backgroundColor: BackgroundColorLight,
+      appBar: secondAppBar("解答之書"),
       body: Stack(
         children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image:
+                    const AssetImage('assets/image/background_answerBook.png'),
+                fit: BoxFit.fill,
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.45), BlendMode.dstIn),
+              ),
+            ),
+          ),
+          /*
           //標題
           Pinned.fromPins(
             Pin(size: 200.0, middle: 0.5),
@@ -44,45 +58,6 @@ class _AnswerbookPageState extends State<AnswerbookPage> {
                 color: const Color(0xffa0522d),
               ),
               softWrap: false,
-            ),
-          ),
-          //抽屜
-          Pinned.fromPins(
-            Pin(size: 41.0, end: 15.0),
-            Pin(size: 36.0, start: 25.0),
-            child: Stack(
-              children: <Widget>[
-                Pinned.fromPins(
-                  Pin(start: 0.0, end: 0.0),
-                  Pin(size: 4.0, start: 0.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xffffbb00),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-                Pinned.fromPins(
-                  Pin(start: 0.0, end: 0.0),
-                  Pin(size: 4.0, middle: 0.5),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xffffbb00),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-                Pinned.fromPins(
-                  Pin(start: 0.0, end: 0.0),
-                  Pin(size: 5.0, end: 0.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xffffbb00),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-              ],
             ),
           ),
           //箭頭
@@ -125,6 +100,7 @@ class _AnswerbookPageState extends State<AnswerbookPage> {
               ),
             ),
           ),
+          */
           //取得解答紐
           Pinned.fromPins(
             Pin(size: 136.0, middle: 0.5),
