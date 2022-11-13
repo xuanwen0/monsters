397b653a67606bb4a3cf31de4d76bce8e810f861
洪靖翔 <seanhong9coding@gmail.com>
2022-11-13 18:55:44 +0800

    monsters new file

---
+++ front-end/monsters_front_end/lib/pages/dev/dev_randomMonster.dart
@@ -0,0 +1,42 @@
+import 'dart:math';
+
+const List monsterNamesList = [
+  "Baku",
+  "Cloud",
+  "Coling",
+  "Donut",
+  "Fuwa",
+  "Ghost",
+  "Ika",
+  "Kila",
+  "Kungfu",
+  "Lizi",
+  "Lory",
+  "Magic",
+  "Pen",
+  "Magic",
+  "Pen",
+  "Pina",
+  "Roy",
+  "Spider",
+  "Sticky",
+  "Three",
+];
+
+String getRandomMonsterName() {
+  return monsterNamesList.elementAt(Random().nextInt(monsterNamesList.length));
+}
+
+String getMonsterAvatarPath(String monsterName) {
+  return 'assets/image/monsters/avatar/Avatar_${monsterName}_PNG.png';
+}
+
+String getMonsterImage(String monsterName) {
+  return 'assets/image/monsters/monster_$monsterName.png';
+}
+
+// getMonsterAvatarPath("Baku","JPG")
+String getMonsterAnimationPath(String monsterName, String direction) {
+  String dir = direction.toLowerCase();
+  return "assets/image/animatedImage/${monsterName}_$dir.gif";
+}
