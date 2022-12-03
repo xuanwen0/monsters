3b308186f093e3dd8ffac15b44c6a81d806b15b1
洪靖翔 <seanhong9coding@gmail.com>
2022-12-03 20:00:04 +0800

    個人資料後端修復

--- back-end/src/main/java/com/example/demo/service/PersonalInfoService.java
+++ back-end/src/main/java/com/example/demo/service/PersonalInfoService.java
@@ -2,6 +2,9 @@ package com.example.demo.service;
 
 import com.example.demo.bean.PersonalInfoBean;
 
+import java.util.List;
+
 public interface PersonalInfoService extends BaseService<PersonalInfoBean>{
     void dailyTest(String account, PersonalInfoBean personalInfoBean);
+    List<PersonalInfoBean> searchPersonalInfoByAccount(String account);
 }
